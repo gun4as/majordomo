2dc4850d17c0e2bcb3ef8183e401c697fb091c25
BuildTools <unconfigured@null.spigotmc.org>
2020-02-16 13:21:18 +0200

    add varitable V_AND to ru.php and default.php

--- languages/default.php
+++ languages/default.php
@@ -1391,6 +1391,7 @@ $dictionary = array(
     'CONNECT_DISABLE_SECURE_CONNECTION' => 'Disable secure connection',
     'HOURS_ARRAY'=>'oclock,oclock,oclock',
     'MINUTE_ARRAY'=>'minute,minutes,minutes',
+    'V_AND'=>'and',
 
     'FEATURES_SUPPORTED' => 'Features supported',
     'NO_FEATURES_WARNING' => 'Sorry, no features supported',
@@ -1410,18 +1411,18 @@ $dictionary = array(
     'REMINDER_INTRO' => 'May I remind you that',
 
     'TEST' => 'test',
-    
+
     /* end module names */
 
     /* start array for convert number to string */
-    
+
 	'NUMBER_TO_STRING_1TEN' => array( array('','one','two','three','four','five','six','seven', 'eight','nine'), array('','one','two','three','four','five','six','seven', 'eight','nine')),
 	'NUMBER_TO_STRING_2TEN' => array('ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'),
 	'NUMBER_TO_STRING_TENS' => array(2=>'twenty ',' thirty ',' forty ',' fifty ',' sixty ',' seventy ',' eighty ',' ninety '),
 	'NUMBER_TO_STRING_HUNDRED' => array('','one hundred','two hundred','three hundred','four hundred','five hundred','six hundred', 'seven hundred','eight hundred','nine hundred'),
 	'NUMBER_TO_STRING_UNIT' => array(array(' ' ,' ' , 1), array(' ' ,' point'   ,' point' ,0),array('thousand ',' thousands', 'thousands'     ,1), array('million ',' million ',' millions' ,0), array('billion', 'billion', 'billion',0)),
 	'NUMBER_TO_STRING_NULL' => 'zero',
-	
+
     /* end array for convert number to string  */
 
 
